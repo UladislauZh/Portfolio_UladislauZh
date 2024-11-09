d07598efefbc9e906b147bee48382f871ac454e7
UladislauZh <uladislau.zh@gmail.com>
2024-11-09 23:58:17 +0300

    portfolio

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -1,13 +1,42 @@
 import React from "react";
+import styled from "styled-components";
+import { Logo } from "../../components/logo/Logo";
+import { Menu } from "../../components/menu/Menu";
 
 export const Header = () => {
   return (
-    <header>
-      <a href="">
-        <img src="" alt="" />
-      </a>
-    </header>
+    <StyledHeader>
+      <Logo />
+      <Menu />
+    </StyledHeader>
   );
 };
 
-export default Header;
+const StyledHeader = styled.header`
+  background-color: #d4ffd3;
+  display: flex;
+  justify-content: space-between;
+`;
+
+/*адаптив
+
+<nav>
+<ul>
+  <li>
+    <a href=""></a>
+  </li>
+  <li>
+    <a href=""></a>
+  </li>
+  <li>
+    <a href=""></a>
+  </li>
+  <li>
+    <a href=""></a>
+  </li>
+  <li>
+    <a href=""></a>
+  </li>
+</ul>
+</nav>
+*/
