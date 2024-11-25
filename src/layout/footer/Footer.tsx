52e2f41bab003f2b35670ae0f951d16f8b60c0c4
UladislauZh <uladislau.zh@gmail.com>
2024-11-25 22:04:54 +0300

    protfolio

--- src/layout/footer/Footer.tsx
+++ src/layout/footer/Footer.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { styled } from "styled-components";
-import { Icon } from "../../components/icon/Icon";
 import { FlexWrapper } from "../../components/FlexWrapper";
 import { theme } from "../../styles/Theme";
 import { Social } from "../../components/social/Social";
